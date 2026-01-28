2be0cea3e1f80bf4373caa36c8f59a10856d3a0c
Dirk Kraaijpoel <dirk.kraaijpoel@tno.nl>
2026-01-28 13:29:13 +0100

    updates package structure

---
+++ preseis/__init__.py
@@ -0,0 +1,2 @@
+# Namespace package for PRESEIS tools
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
--- preseis/source_location/__init__.py
+++ preseis/source_location/__init__.py
@@ -1,2 +1,30 @@
-from .source_location import *
-from .plot import *
+# Main functions
+from .source_location import (
+    characterize_spatial_distribution,
+    infer_spatial_distribution,
+    get_active_stations,
+    invert_covariance,
+    get_spatial_moments,
+    get_spatial_point_estimate,
+    eikonal_solve,
+)
+
+# Plotting functions
+from .plot import (
+    source_plot_with_ellipses,
+    covariance_ellipse,
+)
+
+__all__ = [
+    # source_location
+    "characterize_spatial_distribution",
+    "infer_spatial_distribution",
+    "get_active_stations",
+    "invert_covariance",
+    "get_spatial_moments",
+    "get_spatial_point_estimate",
+    "eikonal_solve",
+    # plot
+    "source_plot_with_ellipses",
+    "covariance_ellipse",
+]
